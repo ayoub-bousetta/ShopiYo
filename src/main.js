1b1ffd720cecd2ad0552d3c3c7e18595f56531e8
Aubbusta <aubbusta@gmail.com>
2018-09-21 16:27:06 +0100

    Final_Ver --1.0 {Start implementing CSS}

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 //import VueResource from 'vue-resource'
+import './assets/css/style.css'
 import VueRouter from 'vue-router'
 import Routes from './routes'
 import store from './store/store'
