f2644dd101e4b0544406cdd79721ad12dc040717
Aubbusta <aubbusta@gmail.com>
2018-09-22 22:56:49 +0100

    Final_Ver {All Done -> Ready to use @if DB set}

--- app/Http/Controllers/ShopsControllers.php
+++ app/Http/Controllers/ShopsControllers.php
@@ -15,11 +15,8 @@ class ShopsControllers extends Controller
     function index(Request $request,$setdistance=null){
 
 
-            
-               
 
-
-        $ip = geoip()->getLocation('41.250.158.62'); //$request->ip()); -> for dynamic on server
+        $ip =$request->ip();  //geoip()->getLocation('41.250.158.62'); -> for Static/local on server
        
         $lat = $ip->lat;
         $lng =$ip->lon;
@@ -49,34 +46,50 @@ class ShopsControllers extends Controller
         }else{
 
            //Table Aka Shop holder
+          
            $shops=[];
-  
+           $shopidsDown=[];
           foreach ($nearShops as $key => $nearshop) {
-
-
-            $shopids=[];
-                $shopidsDown=[];
-
+            
+          
             if (Auth::check()) {
 
                 
 
                 $userVotes=Vote::all()->where('user_id', Auth::user()->id);
-                
 
              
-
+                //Take off from the Home page if liked
                 if($userVotes->where('vote', '=', 1)->count() > 0){
 
+
+                    //Get That i liked
                     foreach ($userVotes->where('vote', '=', 1) as $userVote) {
                         $shopids[]=$userVote->shop_id;
                     }
+
+
+                    //Get That i hate
+                    if ($userVotes->where('vote', '=', -1)->count() > 0) {
+
+                        $data=$userVotes->where('vote', '=', -1);
+                        foreach ($data as $userVote) {
+                            $shopidsDown[]=$userVote->shop_id;
+                        }
+
+                    }
+
+
                    
                     $findshop=Shop::with('vote')->where('id','=',$nearshop->id)->whereNotIn('id', $shopids)->get();
+
+
                 }else{
 
                     if ($userVotes->where('vote', '=', -1)->count() > 0) {
-                        foreach ($userVotes->where('vote', '=', -1) as $userVote) {
+
+                        $data=$userVotes->where('vote', '=', -1);
+                        foreach ($data as $userVote) {
                             $shopidsDown[]=$userVote->shop_id;
                         }
 
@@ -86,21 +99,16 @@ class ShopsControllers extends Controller
                     
                 }
 
-
-                
-               
-
-         
-
             }else{
                 $findshop=Shop::with('vote')->where(['id'=>$nearshop->id])->get();
 
             }
 
   
-                   
-            
 
+                //Desliked shop Yes or no
+
+            $Desliked= (count($shopidsDown)>0 && in_array($nearshop->id,$shopidsDown)) ? 1 : 0 ;
 
                
                if ($findshop->count()>0) {
@@ -111,12 +119,12 @@ class ShopsControllers extends Controller
 
 
 
-              $findshop->map(function ($findshop) use ($nearshop,$likesDes,$shopidsDown) {
+              $findshop->map(function ($findshop) use ($nearshop,$likesDes,$Desliked) {
                $findshop['distance'] = round($nearshop->distance);
                $findshop['likes_count'] = $likesDes['likes']->count();
                $findshop['dislikes_count'] = $likesDes['dislikes']->count();
                $findshop['vote_count'] = $likesDes['dislikes']->count() + $likesDes['likes']->count();
-               $findshop['shops_i_hate'] =  $shopidsDown;
+               $findshop['shops_i_hate'] =   $Desliked;
 
 
            
